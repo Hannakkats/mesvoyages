76bb327ee2ecd523b15e8aa6bae21c3cb5a2f246
Hanna Kats <hannakats2005@gmail.com>
2024-10-30 12:29:24 +0100

    Phase 8:ORM(tri des enregistrement).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -16,7 +16,35 @@ class VoyagesController extends AbstractController {
 
     #[Route('/voyages', name: 'voyages')]
     public function index(): Response {
-        $visites = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("pages/voyages.html.twig",[
+            'visites' =>$visites        
+        ]);
+    }
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response {
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig",[
+            'visites' =>$visites        
+        ]);
+    }
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.entre')]
+    public function entre($champ, $ordre): Response {
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig",[
+            'visites' =>$visites        
+        ]);
+    }
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.venu')]
+    public function venu($champ, $ordre): Response {
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig",[
+            'visites' =>$visites        
+        ]);
+    }
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.arrive')]
+    public function arrive($champ, $ordre): Response {
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
         return $this->render("pages/voyages.html.twig",[
             'visites' =>$visites        
         ]);
